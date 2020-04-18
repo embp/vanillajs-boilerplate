a626700b123596ab63c14283ef3cdd94e4c0bbc6
Daniel Montano <daniel@embusinessproducts.com>
2020-04-18 19:16:53 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # vanillajs-boilerplate
+
 A boilerplate for your vanillajs applications
+
+## How do you use it
+
+1. Download the boilerplate
+1. Run `npm install`
+1. To develop locally run `npm run dev`
+1. To build your project for a production site, run `npm run build`
+1. The end
+
+## How does this boilerplate help you?
+
+This boilerplate comes with webpack pre-configured to do the following:
+
+-   Run a local dev server
+-   Build a production bundle for deploying an optimized build to a live server
+-   Copies your images to the dev and production bundle
+-   Copies your favicon files and site.webmanifest to the bundle
+-   Allows you to use modern Javascript and transpiles to es5 using Babel
+
+In addition to what webpack can do for you, I also included:
+
+-   The [Tachyons](https://tachyons.io/) minimalist css library
+-   [Axios](https://github.com/axios/axios) for making API requests
+-   Includes an example setup for loading images in html and css
+-   [Prettier](https://prettier.io/) to keep your code formatted. Also added a minimal `.prettierrc` file to configure the formatter
+
+## Why did you build this?
+
+I wanted to build a landing page, so I first bought a landing page pack for React. I couldn't beleive how complex and heavy it was just for a single page landing page. I was a little frustrated, so I just built my own landing page with the most basic tools I could (plain Javascript, css, and Tachyons).
+
+After I built my landing page I cleand up my setup a bit and created this boilerplate for future projects. I wanted to share it with other devs that wanted something simple.
+
+## What does this boilerplate not do for you?
+
+This library is for small simple sites. It uses only one webpack config. On more complex sites you may need two.
+
+SASS is not supported out of the box. The idea was to use vanilla js and also just plain css.
+
+## How do I update the favicon setup to add my own icon?
+
+The easiest thing you can do is go to a site like [favicon.io](https://favicon.io/), drag a large image you want to use as an icon, and download the generated assets. Then just drag them into your `src` file and replace all the files. That's all you need to do.
---
+++ webpack.config.js
@@ -0,0 +1,101 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+require('@babel/register');
+
+const config = {
+    entry: ['@babel/polyfill', './src/index.js'],
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: { minimize: true }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [{
+                  loader: 'file-loader',
+                  options: {
+                    // Allows url image references in css to work
+                    esModule: false,
+                  },
+                }]
+            },
+            {
+                test: /\.webmanifest$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            file: './index.html'
+        }),
+        new CopyPlugin(
+            [
+                { from: './src/site.webmanifest', to: './', flatten: true },
+                { from: './src/*.png', to: './', flatten: true },
+                { from: './src/favicon.ico', to: './', flatten: true }
+            ],
+            {
+                copyUnmodified: true
+            }
+        )
+    ],
+    resolve: {
+        modules: [path.resolve('./src'), path.resolve('./node_modules')]
+    },
+    devServer: {
+        contentBase: __dirname + '/public',
+        compress: true,
+        port: 3000,
+        open: true,
+        stats: {
+            assets: false,
+            children: false,
+            chunks: false,
+            chunkModules: false,
+            colors: true,
+            entrypoints: false,
+            hash: false,
+            modules: false,
+            timings: false,
+            version: false
+        }
+    },
+    watch: false,
+    devtool: 'source-map'
+};
+
+module.exports = config;
